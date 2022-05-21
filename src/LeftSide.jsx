6d4321dacb7763039d2ead70daf94a76d668ad4c
Shubhkirti <89776606+Subhkirti@users.noreply.github.com>
2022-05-21 09:13:08 +0530

    Update LeftSide.jsx

--- src/LeftSide.jsx
+++ src/LeftSide.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { Home } from '@material-ui/icons'
 import { Assignment } from '@material-ui/icons'
 import { Group } from '@material-ui/icons'
+import { useStoreState } from 'easy-peasy'
 import { Storefront,Person,Settings,AccountBox,Brightness2} from '@material-ui/icons'
-import { ToggleOn } from '@material-ui/icons'
-function LeftSide() {
+function LeftSide(){
+  const styling=useStoreState((e)=>e)
+  const [color,setColor]=useState(true)
   return (
-    <div className='left'>
+    <div className='left' style={color?styling:{backgroundColor:"black",color:"white",transition:"0.6s"}}>
         <li><Home></Home> Homepage</li>
         <li><Assignment></Assignment> Pages</li>
         <li><Group></Group> Groups</li>
@@ -15,7 +16,12 @@ function LeftSide() {
         <li><Person></Person> Friends</li>
         <li><Settings></Settings> Settings</li>
         <li><AccountBox></AccountBox> Profile</li>
-        <li><Brightness2></Brightness2> <ToggleOn ></ToggleOn></li>
+        <li><Brightness2></Brightness2>
+        <label class="switch">
+          <input type="checkbox"></input>
+          <span class="slider round" onClick={()=>setColor(!color)}></span>
+        </label>
+        </li>
     </div>
   )
 }
